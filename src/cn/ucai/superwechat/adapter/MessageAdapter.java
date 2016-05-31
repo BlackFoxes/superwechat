7735fae327c66c2f73daaf9649489c0058126cf1
BlackFoxes <3296895445@qq.com>
2016-05-31 15:05:26 +0800

    群聊界面头像的单击事件显示详细信息（非好友，非单聊）

--- src/cn/ucai/superwechat/adapter/MessageAdapter.java
+++ src/cn/ucai/superwechat/adapter/MessageAdapter.java
@@ -590,6 +590,7 @@ public class MessageAdapter extends BaseAdapter{
 				Intent intent = new Intent();
 				intent.setClass(context, UserProfileActivity.class);
 				intent.putExtra("username", message.getFrom());
+				intent.putExtra("gourpId", username);
 				context.startActivity(intent);
 			}
 		});
